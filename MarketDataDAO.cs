0458d8992173c97bd03e3b390eb22f4eaa5ed6f0
niamhcassidy <32623758+niamhcassidy@users.noreply.github.com>
2018-03-28 17:47:04 +0000

    Add files via upload

--- MarketDataDAO.cs
+++ MarketDataDAO.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
 
-namespace Test3
-{
-    class MarketDataDAO
+namespace Test
+{ 
+    public class MarketDataDAO
     {
-        
+
         public List<MarketData> AllMarketData()
         {
             Console.WriteLine("hello1");
             Thread.Sleep(1000);
-            List<MarketData> values = File.ReadAllLines("C:\\Users\\Administrator\\Documents\\Insight Challange\\Attempt 2\\Citi.InsightWeek-master (1)\\Citi.InsightWeek-master\\data.csv\\data.csv\\NASDAQ_20160314.csv")
+            List<MarketData> values = File.ReadAllLines("C:\\Users\\Administrator\\Desktop\\marketData\\NASDAQ_20160314.csv")
                                              .Skip(1)
                                              .Select(v => MarketData.FromCsv(v))
                                              .ToList();
@@ -26,5 +26,5 @@ namespace Test3
         }
     }
 
-    
+
 }
