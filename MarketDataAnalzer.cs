aa29b7b33f38ec1c789956ab8c1d88643d90cf82
niamhcassidy <32623758+niamhcassidy@users.noreply.github.com>
2018-03-28 17:46:36 +0000

    Add files via upload

---
+++ MarketDataAnalzer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test
+{
+    public class MarketDataAnalyzer
+    {
+        public List<string> getBestPerformers(List<MarketData> MarketDataList)
+        {
+            List<string> output = new List<string>();
+
+            string topPerformer = "";
+            double topDiff = 0.00;
+
+            double diff = 0.00;
+
+            for (var i = 0; i < MarketDataList.Count; i++)
+            {
+                double open = MarketDataList[i].Open;
+                double close = MarketDataList[i].Close;
+
+                diff = close - open;
+
+                if (diff > topDiff)
+                {
+                    topDiff = diff;
+                    topPerformer = MarketDataList[i].Ticker;
+                }
+            }
+
+
+            output.Add(topPerformer);
+            return output;
+        }
+
+
+    }
+}
