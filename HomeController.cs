69035ea2dac4d52c09bffefd84c14740547fa6a1
niamhcassidy <32623758+niamhcassidy@users.noreply.github.com>
2018-03-28 17:45:35 +0000

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MarketDataDAO dao = new MarketDataDAO();
+            List<MarketData> mdList = dao.AllMarketData();
+
+            MarketDataAnalyzer marketDataAna = new MarketDataAnalyzer();
+            List<string> bestPerformers = marketDataAna.getBestPerformers(mdList);
+            //List<string> bestPerformers = new List<string>();
+            //bestPerformers.Add("ASD");
+            //bestPerformers.Add("QWE");
+            //bestPerformers.Add("ZXC");
+
+            string output = "Best Performers: " + System.Environment.NewLine;
+
+            for (var i = 0; i < bestPerformers.Count(); i++) {
+                output +=  bestPerformers[i];
+            }
+
+            ViewBag.Message = output;
+
+            return View();
+
+        }
+        
+    }
+}
