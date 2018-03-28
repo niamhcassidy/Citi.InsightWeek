4dfd524eaecd7b0d5ed9677efa947c66f01920d7
sylwia20 <37832551+sylwia20@users.noreply.github.com>
2018-03-28 13:06:54 +0000

    Add files via upload

---
+++ MarketDataAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerySimpleContactsCRUDApp
+{
+    public class MarketDataAnalyzer
+    {
+        public List<string> getBestPerformers (List<MarketData> MarketDataList) { 
+        List<string> output = new List<string>();
+
+
+
+            output.Add("subjects");
+            output.Add("drinks");
+        
+        
+
+        return output;
+    }
+}
