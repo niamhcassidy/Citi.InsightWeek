23f337fb9ed0bbeab4be3d9f8d2452f007b18aea
sylwia20 <37832551+sylwia20@users.noreply.github.com>
2018-03-28 16:57:26 +0000

    Add files via upload

--- MarketDataAnalyzer.cs
+++ MarketDataAnalyzer.cs
@@ -7,16 +7,49 @@ namespace VerySimpleContactsCRUDApp
 {
     public class MarketDataAnalyzer
     {
-        public List<string> getBestPerformers (List<MarketData> MarketDataList) { 
-        List<string> output = new List<string>();
+        public List<string> getBestPerformers(List<MarketData> MarketDataList) {
+            List<string> output = new List<string>();
+
+            string topPerformer = "";
+            double topDiff = 0.00;
+
+            double diff = 0.00;
+
+            for (var i = 0; i < MarketDataList.Count; i++) {
+                double open = MarketDataList[i].Open;
+                double close = MarketDataList[i].Close;
+
+                diff = close - open;
+
+                if (diff > topDiff) {
+                    topDiff = diff;
+                    topPerformer = MarketDataList[i].Ticker;
+                }
+            }
+
+
+            output.Add(topPerformer);
+            return output;
+            }
+         
+            
+           
 
 
 
-            output.Add("subjects");
-            output.Add("drinks");
+
+
+            //output.Sort();
+            //string best = output[0];
+            //string worse = output[output.Count - 1];
+
+            
+
+            //output.Add("subjects");
+            //output.Add("drinks");
+            
         
         
 
-        return output;
     }
 }
