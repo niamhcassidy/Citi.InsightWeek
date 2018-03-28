17f94f94956dcf0083e4aa3648401fdd93a151b0
sylwia20 <37832551+sylwia20@users.noreply.github.com>
2018-03-28 10:08:26 +0000

    Add files via upload

---
+++ MarketData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerySimpleContactsCRUDApp
+{
+    public class MarketData
+    { 
+        public MarketData(String ticker, String Date, double open, double high, double low, double close, int volume){
+        ticker=ticker;
+        Date=Date;
+        open=open;
+        high=high;
+        low=low;
+        close=close;
+        volume=volume;
+    }
+public String ticker { get; set; }
+public string Name { get; set; }
+public string Email { get; set; }
+public string Phone { get; set; }
+
+}
