842491721d7bc0f541e5352f3a81e5324324de36
ryoshanks <37832537+ryoshanks@users.noreply.github.com>
2018-03-28 18:23:02 +0100

    Add files via upload

--- MarketData.cs
+++ MarketData.cs
@@ -1,29 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VerySimpleContactsCRUDApp
+class MarketData
 {
-    public class MarketData
+    public MarketData(string ticker, string date, double open, double high, double low, double close, int volume
+        )
     {
-        public MarketData(string ticker, string date, double open, double high, double low, double close, int volume)
-        {
-            Ticker = ticker;
-            Date = date;
-            Open = open;
-            High = high;
-            Low = low;
-            Close = close;
-            Volume = volume;
-        }
-        public string Ticker { get; set; }
-        public string Date { get; set; }
-        public double Open { get; set; }
-        public double High { get; set; }
-        public double Low { get; set; }
-        public double Close { get; set; }
-        public int Volume { get; set; }
+        Ticker = ticker;
+        Date = date;
+        Open = open;
+        High = high;
+        Low = low;
+        Close = close;
+        Volume = volume;
+    }
+    public string Ticker;
+    public string Date;
+    public double Open;
+    public double High;
+    public double Low;
+    public double Close;
+    public int Volume;
+
+    public static MarketData FromCsv(string csvLine)
+
+    {
+        string[] values = csvLine.Split(',');
+        MarketData MarketData = new MarketData(Convert.ToString(values[0]), Convert.ToString(values[1]), Convert.ToDouble(values[2]), Convert.ToDouble(values[3]),
+        Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), Convert.ToInt32(values[6]));
+        MarketData.Ticker = Convert.ToString(values[0]);
+        MarketData.Date = Convert.ToString(values[1]);
+        MarketData.Open = Convert.ToDouble(values[2]);
+        MarketData.High = Convert.ToDouble(values[3]);
+        MarketData.Low = Convert.ToDouble(values[4]);
+        MarketData.Close = Convert.ToDouble(values[5]);
+        MarketData.Volume = Convert.ToInt32(values[6]);
+        return MarketData;
+
 
     }
 }
--- MarketDataDAO.cs
+++ MarketDataDAO.cs
@@ -1,79 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
-namespace VerySimpleContactsCRUDApp
+namespace Test3
 {
-    public static class MarketDataDAO
+    class MarketDataDAO
     {
-        private static string strConn = @"user id = root;" + 
-                                       "password=c0nygre;server=localhost:3306;" + 
-                                       "Trusted_Connection=yes;" + 
-                                       "database=test; " + 
-                                       "connection timeout=30";
-        public static List<MarketData> AllMarketData()
+        
+        public List<MarketData> AllMarketData()
         {
-            List<MarketData> MarketDatas = new List<MarketData>();
-            using(SqlConnection cn = new SqlConnection(strConn))
-            {
-                SqlCommand cmd = new SqlCommand("SELECT * FROM MarketData",cn);
-                cn.Open();
-                using (SqlDataReader dr = cmd.ExecuteReader())
-                {
-                    while (dr.Read())
-                    {
-                            MarketDatas.Add(new MarketData(dr.GetString(dr.GetOrdinal("ticker")), 
-                            dr.GetString(dr.GetOrdinal("Date")),
-                            dr.GetDouble(dr.GetOrdinal("Open")),
-                            dr.GetDouble(dr.GetOrdinal("High")),
-                            dr.GetDouble(dr.GetOrdinal("Low")),
-                            dr.GetDouble(dr.GetOrdinal("Close")),
-                            dr.GetInt32(dr.GetOrdinal("Volume"))));
-                    }
-                }
-            }
-            return MarketDatas;
-        }
-        public static void AddContact(Contact c)
-        {
-            using (SqlConnection cn = new SqlConnection(strConn))
-            {
-                SqlCommand cmd = new SqlCommand("INSERT INTO Contact(name,email,phone)" +
-                    " VALUES(@name,@email,@phone)", cn);
-                cmd.Parameters.AddWithValue("@name", c.Name);
-                cmd.Parameters.AddWithValue("@email", c.Email);
-                cmd.Parameters.AddWithValue("@phone", c.Phone);
-                cn.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
+            Console.WriteLine("hello1");
+            Thread.Sleep(1000);
+            List<MarketData> values = File.ReadAllLines("C:\\Users\\Administrator\\Documents\\Insight Challange\\Attempt 2\\Citi.InsightWeek-master (1)\\Citi.InsightWeek-master\\data.csv\\data.csv\\NASDAQ_20160314.csv")
+                                             .Skip(1)
+                                             .Select(v => MarketData.FromCsv(v))
+                                             .ToList();
 
-        public static void UpdateContact(Contact c)
-        {
-            using (SqlConnection cn = new SqlConnection(strConn))
-            {
-                SqlCommand cmd = new SqlCommand("UPDATE Contact SET name = " +
-                    "@name, email = @email, Phone = @phone WHERE id = @id", cn);
-                cmd.Parameters.AddWithValue("@name", c.Name);
-                cmd.Parameters.AddWithValue("@email", c.Email);
-                cmd.Parameters.AddWithValue("@phone", c.Phone);
-                cmd.Parameters.AddWithValue("@id", c.Id);
-                cn.Open();
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        public static void DeleteContact(Contact c)
-        {
-            using (SqlConnection cn = new SqlConnection(strConn))
-            {
-                SqlCommand cmd = new SqlCommand("DELETE Contact WHERE id = @id", cn);
-                cmd.Parameters.AddWithValue("@id", c.Id);
-                cn.Open();
-                cmd.ExecuteNonQuery();
-            }
+            Console.WriteLine("Hello5");
+            Thread.Sleep(1000);
+            return values;
         }
     }
+
+    
 }
